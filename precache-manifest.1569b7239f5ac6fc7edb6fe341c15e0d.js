e710223d6f2e8742c7fd565af7e06be2cac56f37
jschrbr <schrbrr@gmail.com>
2020-03-28 08:33:27 +1100

    Updates

---
+++ precache-manifest.1569b7239f5ac6fc7edb6fe341c15e0d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d392feae18b90e13f578d519e010eb06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d5d2779024d10b5416b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24dd7b603fc9595b83d7",
+    "url": "/static/js/2.8f270b5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8f270b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5d2779024d10b5416b",
+    "url": "/static/js/main.eeecc31f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "a505596aa8fd7248ff771485e61aa0f2",
+    "url": "/static/media/Food Hound.a505596a.gif"
+  },
+  {
+    "revision": "14ccb98adc14c1b849bda7436eb5b361",
+    "url": "/static/media/Node1.14ccb98a.jpg"
+  },
+  {
+    "revision": "5de0e130cd4b128386878780566f9182",
+    "url": "/static/media/Password Generator.5de0e130.gif"
+  },
+  {
+    "revision": "ea253d2559f10df9c1a03be29ae49514",
+    "url": "/static/media/Work Day Scheduler.ea253d25.gif"
+  },
+  {
+    "revision": "21cced8edcd3dfc2af8037d1de0a26e9",
+    "url": "/static/media/background.21cced8e.jpg"
+  },
+  {
+    "revision": "8e815d8b6b33bbfdbedfd0aca1b7ca01",
+    "url": "/static/media/code.8e815d8b.jpeg"
+  },
+  {
+    "revision": "3ab885408e9bf6b811618b68fbd3207f",
+    "url": "/static/media/homer-computer.3ab88540.gif"
+  },
+  {
+    "revision": "e0c1375562085a2c459a0e71a864dd64",
+    "url": "/static/media/me.e0c13755.jpg"
+  },
+  {
+    "revision": "d56c1d336b0196a7f736c36e9bcd48ba",
+    "url": "/static/media/placeholder-image.d56c1d33.jpg"
+  },
+  {
+    "revision": "340e6b446ed1dd1129b553da0fe3e111",
+    "url": "/static/media/robo.340e6b44.jpg"
+  },
+  {
+    "revision": "10470c68e4808510e36fcd77445296d9",
+    "url": "/static/media/sim.10470c68.gif"
+  },
+  {
+    "revision": "0845b930e242c1fc6fcf85fec33d0796",
+    "url": "/static/media/simple-bin.0845b930.png"
+  },
+  {
+    "revision": "e130a2df815a9490bb4d9b565d1875c7",
+    "url": "/static/media/soldering.e130a2df.jpg"
+  },
+  {
+    "revision": "f68a1eddd7d8d35fe2154338af92b548",
+    "url": "/static/media/sparks.f68a1edd.jpg"
+  },
+  {
+    "revision": "90851be0c09df22cdd143d8e3fbf1deb",
+    "url": "/static/media/weather.90851be0.png"
+  }
+]);
